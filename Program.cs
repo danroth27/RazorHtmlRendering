be7886144c93f593091d534a481687ba6d4bb6f2
Daniel Roth <daroth@microsoft.com>
2023-04-14 16:31:04 -0700

    Add static HTML rendering sample

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RazorHtmlRendering;
+
+IServiceCollection services = new ServiceCollection();
+services.AddLogging();
+
+IServiceProvider serviceProvider = services.BuildServiceProvider();
+ILoggerFactory loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+await using var htmlRenderer = new HtmlRenderer(serviceProvider, loggerFactory);
+var html = await htmlRenderer.Dispatcher.InvokeAsync(async () =>
+{
+    var output = await htmlRenderer.RenderComponentAsync<MyComponent>(ParameterView.Empty);
+    return output.ToHtmlString();
+});
+
+Console.WriteLine(html);
